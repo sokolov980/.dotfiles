ff9c65d3af1b9d29137b813fafb4cdd02e59cfd2
Sokolov <227881975+sokolov980@users.noreply.github.com>
2026-01-13 17:27:24 -0500

    Update enable_dnd.sh

--- bin/deepwork/enable_dnd.sh
+++ bin/deepwork/enable_dnd.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env zsh
-# Enable macOS Do Not Disturb
-
-# Set DND to true
-defaults -currentHost write ~/Library/Preferences/ByHost/com.apple.notificationcenterui doNotDisturb -boolean true
-
-# Optionally set a future timestamp (disable auto-off)
-defaults -currentHost write ~/Library/Preferences/ByHost/com.apple.notificationcenterui doNotDisturbDate -date "$(date +%s)"
-
-# Restart NotificationCenter to apply changes
-killall NotificationCenter 2>/dev/null
+# Enable Do Not Disturb using assigned keyboard shortcut
+osascript <<EOF
+tell application "System Events"
+    keystroke space using {command down, control down, option down}
+end tell
+EOF
