f2bc57610b4c5333f86f557d385f404e19a962a0
Sokolov <227881975+sokolov980@users.noreply.github.com>
2026-01-13 16:36:37 -0500

    Update disable_dnd.sh

--- bin/deepwork/disable_dnd.sh
+++ bin/deepwork/disable_dnd.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env zsh
-# Disable Do Not Disturb on macOS
-
+# Disable Do Not Disturb
 defaults -currentHost write ~/Library/Preferences/ByHost/com.apple.notificationcenterui doNotDisturb -boolean false
 defaults -currentHost write ~/Library/Preferences/ByHost/com.apple.notificationcenterui doNotDisturbDate -date "$(date)"
-killall NotificationCenter 2>/dev/null || true
-
+killall NotificationCenter
 echo "[✓] DND disabled"
